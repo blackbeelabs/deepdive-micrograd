652d31e04378eb7b6ce1395916349af216469297
blackbeelabs <blackbeelabs@outlook.com>
2024-04-26 15:24:25 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# For this setup, I use Python 3.10.13
+# For setting up the Python environment. 
+# I was abit lazy to implement a requirements.txt file as it was always changing.
+pip install torch==2.2.1
 pip install notebook==6.4.13
 pip install pendulum==3.0.0
-pip install torch==2.1.2
